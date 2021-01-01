6d450b273584e9e43b4a0f32a104507b4cec0d6e
gmoore52 <gavinmoore5225@gmail.com>
2021-01-01 07:16:34 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+float run1pay, run2pay, runtype1final, runtype2final, cargo, runtype1weight, runtype2weight, runtype1finalpay, runtype2finalpay;
+char uselessvar, mode2;
+string mode1;
+
+int main();
+
+void comparison()
+{
+  cout << "Enter Run type 1's unrefined weight: ";
+  cin >> runtype1weight;
+  cout << "Enter Run type 2's unrefined weight: ";
+  cin >> runtype2weight;
+  cout << "Enter amount of space including character and vehicle: ";
+  cin >> cargo;
+  cout << "Enter Run 1's pay per item: ";
+  cin >> run1pay;
+  cout << "Enter Run 2's pay per item: ";
+  cin >> run2pay;
+  runtype1final = cargo / runtype1weight;
+  runtype2final = cargo / runtype2weight;
+  runtype1finalpay = runtype1final * run1pay;
+  runtype2finalpay = runtype2final * run2pay;
+  cout << "Run 1 will make: $" << runtype1finalpay << endl << "Run 2 will make: $" << runtype2finalpay << endl;
+  cout << "Enter Y to restart the program, press any other button to close the program: ";
+  cin >> uselessvar;
+  switch(uselessvar)
+  {
+    case 'Y':
+      main();
+      break;
+    case 'y':
+      main();
+      break;
+    default:
+      cout << "ok";
+  }
+}
+
+void calc()
+{
+  cout << "Enter run unrefined weight: ";
+  cin >> runtype1weight;
+  cout << "Enter run pay per item: ";
+  cin >> run1pay;
+  cout << "Enter amount of space including character and vehicle: ";
+  cin >> cargo;
+  runtype1final = cargo / runtype1weight;
+  runtype1finalpay = runtype1final * run1pay;
+  cout << "This run will make: $" << runtype1finalpay << endl;
+  cout << "Enter Y to restart the program, press any other button to close the program: ";
+  cin >> uselessvar;
+  switch(uselessvar)
+  {
+    case 'Y':
+      main();
+      break;
+    case 'y':
+      main();
+      break;
+    default:
+      cout << "ok";
+  }
+}
+
+int main()
+{
+  mode1 = "1";
+  mode2 = '1';
+  cout << "Comparison or Calculator? 1 for Comparison, 2 for Calculator: ";
+  cin >> mode1;
+  if(mode1.length() != 1)
+  {
+    cout << "Please enter a valid answer" << endl;
+    main();
+  }
+  else
+  {
+    mode2 = mode1[0];
+    switch(mode2)
+    {
+    case '1':
+      comparison();
+      break;
+    case '2':
+      calc();
+      break;
+    default:
+      cout << "Please enter a valid answer" << endl;
+      main();
+    }
+  }
+}
