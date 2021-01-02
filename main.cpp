a7266dc432b594a5cd82bdb5edcc7c568cd8201c
gmoore52 <gavinmoore5225@gmail.com>
2021-01-02 22:30:57 +0000

    Added WIP code editing the comparison function to support the new runtax function

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <string>
 using namespace std;
 
-int run1pay, run2pay, runtype1final, runtype2final, cargo, runtype1weight, runtype2weight, runtype1finalpay, runtype2finalpay, moonmoney, moontruck, moonmoneyfinal, moonplacer, methmoney, methtruck, methmoneyfinal, methplacer, methtax, methdifference, moontax, moondifference;
+int run1pay, run2pay, runtype1final, runtype2final, cargo, runtype1weight, runtype2weight, runtype1finalpay, runtype2finalpay, moonmoney, moontruck, moonmoneyfinal, moonplacer, methmoney, methtruck, methmoneyfinal, methplacer, methtax, methdifference, moontax, moondifference, bonusmoney, runtype1tax, runtype2tax, runtype1diff, runtype2diff;
 char uselessvar, mode2, legallity, license;
 string mode1, uselessvar1, legallity1, license1;
 bool legal, wpl;
@@ -16,36 +16,53 @@ void runtax()
   cin >> legallity1;
   if(legallity1.length() != 1)
   {
-    cout << "ok";
+    cout << "Please enter a valid answer" << endl;
+    runtax();
   }
   else
   {
     legallity = legallity1[0];
-  }
-  switch(legallity)
-  {
-    case '1':
-      cout << "Do you have a workers protection license? 1 for yes, 2 for no: ";
-      cin >> license1;
-      if(license1.length() != 1)
-      {
+    switch(legallity)
+    {
+      case '1':
+        legal = true;
+        cout << "Do you have a workers protection license? 1 for yes, 2 for no: ";
+        cin >> license1;
+        if(license1.length() != 1)
+        {
+          cout << "Please enter a valid answer" << endl;
+         runtax();
+        }
+        else
+        {
+          license = license1[0];
+          switch(license)
+          {
+            case '1':
+              bonusmoney = 15 / 10;
+              wpl = true;
+              break;
+            case '2':
+              bonusmoney = 0;
+              wpl = false;
+              break;
+            default:
+              cout << "Please enter a valid answer" << endl;
+              runtax();
+            }
+          }
+        break;
+      case '2':
+        bonusmoney = 0;
+        legal = false;
+        break;
+      default:
         cout << "Please enter a valid answer" << endl;
         runtax();
-      }
-      else
-      {
-        license = license1[0];
-        switch(license)
-        {
-          
-        }
-      }
-      break;
-    case '2':
-
-      break;
       
+    }
   }
+  
   if(legallity == '1')
   {
     legal = true;
@@ -140,21 +157,41 @@ void moonshine()
 
 void comparison()
 {
-  cout << "Enter Run type 1's unrefined weight: ";
-  cin >> runtype1weight;
-  cout << "Enter Run type 2's unrefined weight: ";
-  cin >> runtype2weight;
   cout << "Enter amount of space including character and vehicle: ";
   cin >> cargo;
+  cout << "Enter Run type 1's unrefined weight: ";
+  cin >> runtype1weight;
   cout << "Enter Run 1's pay per item: ";
   cin >> run1pay;
+  if(legal == true)
+  {
+    runtype1final = cargo / runtype1weight;
+    runtype1finalpay = runtype1final * run1pay;
+    if(wpl == true)
+    {
+      runtype1tax = runtype1finalpay * bonusmoney;
+      runtype1diff = runtype1tax - runtype1finalpay;
+      cout << "This run will make $" << runtype1tax << " with workers protection." << endl;
+      cout << "That is a bonus amount of $" << runtype1diff << endl;
+    }
+    else
+    {
+      cout << "Run 1 will make: $" << runtype1finalpay << endl;
+    }
+  }
+  else
+  {
+    runtype1tax = runtype1finalpay * 8.5 / 10;
+    runtype1diff = runtype1finalpay - runtype1tax;
+    cout << "This run will make $" << runtype1tax << " after cartel tax and $" << runtype1finalpay << " before tax" << endl << "That is a difference of $" << runtype1diff << endl;
+  }
+  cout << "Enter Run type 2's unrefined weight: ";
+  cin >> runtype2weight;
   cout << "Enter Run 2's pay per item: ";
   cin >> run2pay;
-  runtype1final = cargo / runtype1weight;
   runtype2final = cargo / runtype2weight;
-  runtype1finalpay = runtype1final * run1pay;
   runtype2finalpay = runtype2final * run2pay;
-  cout << "Run 1 will make: $" << runtype1finalpay << endl << "Run 2 will make: $" << runtype2finalpay << endl;
+  cout << "Run 2 will make: $" << runtype2finalpay << endl;
   restart();
 }
 
