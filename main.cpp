d1ccdcf24986ec6de08613c9f71d64f45d6d5ad5
gmoore52 <gavinmoore5225@gmail.com>
2021-01-02 21:49:34 +0000

    added WIP code of the auto tax calculator function

--- main.cpp
+++ main.cpp
@@ -4,10 +4,84 @@
 using namespace std;
 
 int run1pay, run2pay, runtype1final, runtype2final, cargo, runtype1weight, runtype2weight, runtype1finalpay, runtype2finalpay, moonmoney, moontruck, moonmoneyfinal, moonplacer, methmoney, methtruck, methmoneyfinal, methplacer, methtax, methdifference, moontax, moondifference;
-char uselessvar, mode2;
-string mode1, uselessvar1;
+char uselessvar, mode2, legallity, license;
+string mode1, uselessvar1, legallity1, license1;
+bool legal, wpl;
 
 int main();
+
+void runtax()
+{
+  cout << "Enter 1 if this run is legal, enter 2 if this run is illegal: ";
+  cin >> legallity1;
+  if(legallity1.length() != 1)
+  {
+    cout << "ok";
+  }
+  else
+  {
+    legallity = legallity1[0];
+  }
+  switch(legallity)
+  {
+    case '1':
+      cout << "Do you have a workers protection license? 1 for yes, 2 for no: ";
+      cin >> license1;
+      if(license1.length() != 1)
+      {
+        cout << "Please enter a valid answer" << endl;
+        runtax();
+      }
+      else
+      {
+        license = license1[0];
+        switch(license)
+        {
+          
+        }
+      }
+      break;
+    case '2':
+
+      break;
+      
+  }
+  if(legallity == '1')
+  {
+    legal = true;
+  }
+  else if(legallity == '2')
+  {
+    legal = false;
+  }
+  else
+  {
+    cout << "Please enter a valid answer" << endl;
+    runtax();
+  }
+  if(legal == true)
+  {
+    cout << "Do you have a workers protection license? 1 for yes, 2 for no: ";
+    cin >> license1;
+    if(license1.length() != 1)
+    {
+    cout << "Please enter a valid answer" << endl;
+    runtax();
+    }
+    else
+    {
+    license = license1[0];
+    }
+    if(license == '1')
+    {
+      wpl = true;
+    }
+    else
+    {
+      wpl = false;
+    }
+  }
+}
 
 void restart()
 {
